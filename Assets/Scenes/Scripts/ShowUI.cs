96419c678f0bcc7b0767c4af55d72275ff1f8fd7
Juliane98 <74611516+Juliane98@users.noreply.github.com>
2021-01-23 15:03:45 +0100

    Versuch Timer Gegner

--- Assets/Scenes/Scripts/ShowUI.cs
+++ Assets/Scenes/Scripts/ShowUI.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowUI : MonoBehaviour
 {
 
     public GameObject uiObject;
+    public GameObject timerEnemies;
     public bool playerOnPlattform;
+
+    public int countdownTimeEnemies;
+    public TextMesh countdownTextEnemies;
 
     void Start()
     {
         uiObject.SetActive(false);
+        timerEnemies.SetActive(false);
         playerOnPlattform =  false;
     }
 
@@ -21,7 +27,9 @@ public class ShowUI : MonoBehaviour
         {
             playerOnPlattform = true;
             uiObject.SetActive(true);
+            timerEnemies.SetActive(true);
             StartCoroutine("WaitForSec");
+            StartCoroutine(CountdownEnemies());
         }
     }
     IEnumerator WaitForSec()
@@ -29,5 +37,22 @@ public class ShowUI : MonoBehaviour
         yield return new WaitForSeconds(5);
         Destroy(uiObject);
         
+    }
+
+    IEnumerator CountdownEnemies()
+    {
+        while (countdownTimeEnemies > 0)
+        {
+            countdownTextEnemies.text = countdownTimeEnemies.ToString();
+
+            yield return new WaitForSeconds(1f);
+
+            countdownTimeEnemies--;
+        }
+
+        countdownTextEnemies.text = "GOOD JOB!";
+
+        yield return new WaitForSeconds(1f);
+        Destroy(timerEnemies);
     }
 }
