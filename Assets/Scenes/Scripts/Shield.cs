5b705ce03523872c3c0db99d6862f073bd480ab8
alexB1705 <74549699+alexB1705@users.noreply.github.com>
2021-01-27 14:47:20 +0100

    Flag + Shield

---
+++ Assets/Scenes/Scripts/Shield.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shield : MonoBehaviour
+{
+    [SerializeField]
+    public Transform Player;
+
+    public Texture SampleTexture;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            FindObjectOfType<AudioManager>().Play("Clock");
+            Destroy(this.gameObject);
+            Player.GetComponent<Renderer>().material.SetColor("_Color", Color.cyan);
+        }
+    }
+
+}
