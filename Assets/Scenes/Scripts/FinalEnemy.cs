43d1ff375084f7b42a7405e204208e1e05180fd4
Juliane98 <74611516+Juliane98@users.noreply.github.com>
2021-03-18 15:19:27 +0100

    L06 Text Final Enemy

--- Assets/Scenes/Scripts/FinalEnemy.cs
+++ Assets/Scenes/Scripts/FinalEnemy.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FinalEnemy : MonoBehaviour
 {
+    public Text textFinalEnemy;
 
     public bool playerOnEndPlatform;
+
+    void Start()
+    {
+        textFinalEnemy.gameObject.SetActive(false);
+    }
+
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Player")
         {
             playerOnEndPlatform = true;
+            StartCoroutine(showDefeatText());
         }
     }
+
+    IEnumerator showDefeatText()
+    {
+        textFinalEnemy.gameObject.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        textFinalEnemy.gameObject.SetActive(false);
+    }
+    
 }
